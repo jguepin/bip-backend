7db9fb59d2c33414938bad305a865f99f7a68e70
Julien Guépin <julien@guepin.fr>
2014-10-16 01:43:07 +0200

    New Google API key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/town',
-  googleApiKey: process.env.GOOGLE_KEY || 'AIzaSyDMhfGswnWaXyBBDLX_UEJy73bBKtklFlI',
+  googleApiKey: process.env.GOOGLE_KEY || 'AIzaSyCqsC2DXaAFg8xVBhLBeudM944GiTuGF8Q',
   foursquare: {
     clientId: process.env.FOURSQUARE_CLIENT_ID || 'VW2JSE3TSAIYMX410V1SNJJEEGEEYKZ0CV5PJK0XFIOLJVRP',
     clientSecret: process.env.FOURSQUARE_CLIENT_SECRET || 'YO0D1ITR4JC5HSJXBZWTAO3XBGHWH11CCRG4KYIRTD0ECLAM',
